7b0a4b611990b475ea6610dd2a6a0b71624c8c9b
Geek University <53379890+guniversityBR@users.noreply.github.com>
2024-07-01 14:19:53 +0100

    Update asgi.py
    Updating following the docs

--- realtime/asgi.py
+++ realtime/asgi.py
@@ -8,9 +8,25 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
-
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from chat.routing import websocket_urlpatterns
+ 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realtime.settings')
+ 
+# Regular Django ASGI application
+django_asgi_application = get_asgi_application()
+ 
+# Django Channels WebSocket application
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_application,
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                websocket_urlpatterns
+            )
+        ),
+    }
+)
 
-application = get_asgi_application()
